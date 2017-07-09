7bfc13752e4dd02309f72aea40f3a535bcf2560d
Keerthana Sriranga <keerthanasriranga11@gmail.com>
2017-07-09 10:21:20 +0530

    Add files via upload

---
+++ Amogh - Learn and Grow/practice.php
@@ -0,0 +1,121 @@
+<HTML>
+<BODY style="margin-left: 100px;">
+<h1>Practice Paper</h1>
+<h3>Type each of the following</h3>
+                <fieldset style="width: 600px;">
+                                    <h3>Apple</h3>
+                    <input type="text">
+                    <p style="color: red;">hint. Apple</p>
+                
+                
+  
+                
+                    <h3>Cat</h3>
+                    <input type="text">
+                    <p style="color: red;">hint. Cat</p>
+                
+                
+                
+                <p><b>Address</b></p>
+                <p>900,Commercial Street,Bangalore,Karnataka,India
+                
+
+                
+            
+                    <h3>Door Number</h3>
+                    
+                    <div>
+                        <input type="text" name="question-1-answers" id="question-1-answers-A"/>
+                        <label for="question-1-answers-A"></label>
+                         <p style="color: red;">hint. 729</p>
+                    </div>
+                    
+                
+                
+                
+                
+                    <h3>Street Name</h3>
+                    
+                     <div>
+                        <input type="text" name="question-2-answers" id="question-1-answers-A"/>
+                        <label for="question-1-answers-A"></label>
+                         <p style="color: red;">Commercial Street</p>
+                    </div>
+                
+                
+                
+                
+                
+                    <h3>City</h3>
+                    
+                     <div>
+                        <input type="text" name="question-3-answers" id="question-1-answers-A"/>
+                        <label for="question-1-answers-A"></label>
+                         <p style="color: red;">hint. Bangalore</p>
+                    </div>
+                
+                
+                
+                
+                    <h3>State</h3>
+                    <div>
+                        <input type="text" name="question-4-answers" id="question-1-answers-A"/>
+                        <label for="question-1-answers-A"></label>
+                         <p style="color: red;">hint. Karnataka</p>
+                    </div>
+                   
+                
+                
+                
+                
+                
+                    <h3>Country</h3>
+                    
+                     <div>
+                        <input type="text" name="question-5-answers" id="question-1-answers-A"/>
+                        <label for="question-1-answers-A"></label>
+                         <p style="color: red;">hint. India</p>
+                    </div>
+                
+                
+                
+                    <h3>Banana</h3>
+                    <input type="text">
+                    <p style="color: red;">hint. Banana</p>
+                
+                
+                
+                	
+                
+                    <h3>House</h3>
+                    <input type="text">
+                    <p style="color: red;">hint. House</p>
+                
+                
+                
+                
+                
+                    <h3>Chin</h3>
+                    <input type="text">
+                    <p style="color: red;">hint. Chin</p>
+                
+                
+                
+                
+            <div>
+
+
+                
+               
+                
+                    <h3><?php echo $smalla[$small2]; ?></h3>
+                    <div>
+                        <input type="text" name="question-4-answers" id="question-1-answers-A"/>
+                        <label for="question-1-answers-A"></label>
+                    </div>
+                   
+                
+                </fieldset>
+
+                </BODY>
+</HTML>
